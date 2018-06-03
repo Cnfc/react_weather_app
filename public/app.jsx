50de0752f49c52ebd722d236ce49a1e4a85b82e7
cnfc <stdashkov@mail.ru>
2018-06-03 19:28:39 +0300

    state component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,114 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'This is default message'
+    };
+  },
+
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+
+  onButtonClick: function(e){
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = this.refs.name.value;
+    nameRef.value="";
+
+    if(typeof name === 'string' && name.length>0 ) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+
+
+  render: function() {
+    var name = this.state.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <h2>{message} !!  </h2>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Andrew';
+
 ReactDOM.render(
-  <h1>Hello react!</h1>,
+  <Greeter name = {firstName}/>,
   document.getElementById('app')
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
