1993edbc16c8ffa233fe6c9ae7f17b096e83f227
cnfc <stdashkov@mail.ru>
2018-06-05 21:29:56 +0300

    Declar. clock

--- public/app.jsx
+++ public/app.jsx
@@ -1,58 +1,75 @@
 
-// Every seconds
-setInterval(logClockTime, 1000);
+const oneSecond = () => 1000;
+const getCurrentTime = () => newDate();
+const clear = () => console.clear();
+const log = message => console.log(message);
 
-function logClockTime () {
-  let time = getClockTime();
 
-  console.clear();
-  console.log(time);
-}
-
-function getClockTime(){
-  //Current time
-  var date = new Date();
-  var time = '';
-
-  var time = {
+const serializeClockTime = date =>
+  ({
     hours: date.getHours(),
     minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-    ampm: "AM"
-  }
+    seconds: date.getSeconds()
+  });
 
-  if(time.hours == 12) {
-    time.ampm = "PM";
-  } else if(time.hours > 12) {
-    time.ampm = "PM";
-    time.hours -= 12;
-  }
-  if(time.hours < 10) {
-    time.hours = "0" + time.hours;
-  }
+const civilianHours = clockTime =>
+  ({
+    ...clockTime,
+    hours: (clockTime.hours>12) ?
+      clockTime.hours -12:
+      clockTime.hours
 
-  if(time.minutes< 10) {
-    time.minutes = '0' + time.minutes;
-  }
+  })
+const appendAMPM = clockTime =>
+  ({
+    ...clockTime,
+    ampm: (clockTime.hours >= 12) ? "PM": "AM"
+  })
 
-  if(time.seconds<10) {
-    time.seconds = '0' + time.seconds;
-  }
-  return time.hours+":"
-  + time.minutes + ':'
-  + time.seconds + ':'
-  + time.ampm;
+const display = target => time => target(time)
+const formatClock = format =>
+  time =>
+    format.replace("hh", time.hours)
+    .replace("mm", time.minutes)
+    .replace("ss", time.seconds)
+    .replace("tt", time.ampm)
 
-}
-
-
-
-
-
-
-
+const pretendZero = key => clockTime =>
+  ({
+    ...clockTime,
+    [key]: (clockTime[key] <10 ) ?
+      "0" + clockTime[key] :
+      clockTime[key]
+  })
 
 
+  const convertToCivilianTime = clockTime =>
+    compose(
+      appendAMPM,
+      civilianHours
+    )(clockTime)
+
+const doubleDigits = civilianTime =>
+  compose(
+    prependZero("hours"),
+    prependZero("minutes"),
+    prependZero("seconds")
+  )(civilianTime)
+
+const startTicking = () =>
+  setInterval(
+    compose (
+      clear,
+      getCurrentTime,
+      serializeClockTime,
+      convertToCivilianTime,
+      doubleDigits,
+      formatClock("hh:mm:ss tt"),
+      display(log)
+    ),
+    oneSecond()
+)
+  startTicking()
 
 
 
