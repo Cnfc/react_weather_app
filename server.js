a933ece8f4499330a512710d346f93cd4a28c7d6
cnfc <stdashkov@mail.ru>
2018-06-06 18:07:32 +0300

    add depend.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 
+
 // Create app
 var app = express();
 
 app.use(express.static('public'));
-app.listen(3000, function() {
+app.listen(3000, functio n() {
   console.log("Express server is up on port 3000");
 });
