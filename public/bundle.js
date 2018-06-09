8706b8f3fe0487310f45fd55c972306a72eab5a2
cnfc <stdashkov@mail.ru>
2018-06-09 18:10:10 +0300

    about page

--- public/bundle.js
+++ public/bundle.js
@@ -26728,23 +26728,53 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(7);
 
-	// var About = React.createClass({
-	//   render: function(){
-	//     return(
-	//       <h2>About components</h2>
-	//     )
-	//   }
-	// });
-
 	var About = function About(props) {
 	  return React.createElement(
-	    'h2',
+	    "div",
 	    null,
-	    'About components'
+	    React.createElement(
+	      "h1",
+	      { className: "text-center" },
+	      "About"
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "This is a weather application build on React. I have built this for The Complete React Web App Developer Course."
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Here are some of the tools I used:"
+	    ),
+	    React.createElement(
+	      "ul",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://facebook.github.io/react" },
+	          "React"
+	        ),
+	        " - This was the JavaScript framework used."
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "http://openweathermap.org" },
+	          "Open Weather Map"
+	        ),
+	        " - I used Open Weather Map to search for weather data by city name."
+	      )
+	    )
 	  );
 	};
 
