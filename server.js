20cb9e5b482802dc20e2b4c2de15ed9c25e8153c
cnfc <stdashkov@mail.ru>
2018-06-09 14:41:08 +0300

    x-for

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(function (req, res, next) {
-  if(req.headers['x-forwarded-photo'] === 'http') {
+  if(req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
 app.use(express.static('public'));
-
-
 
 app.listen(PORT, function() {
   console.log("Express server is up on port " + PORT);
